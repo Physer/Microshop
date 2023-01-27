da39e82f3cc3ec3769d917ea9d1a99b96d14bf47
Alex Schouls <alex.schouls@valtech.com>
2023-01-27 15:57:17 +0100

    Added web project

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <head />
+            <body>{children}</body>
+        </html>
+    );
+}
