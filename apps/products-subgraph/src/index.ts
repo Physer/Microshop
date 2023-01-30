5bcb8def96a06d63f2c1417ed4847b6ee5f75465
Alex Schouls <alex.schouls@valtech.com>
2023-01-30 16:27:08 +0100

    feat(products): add standalone Apollo GraphQL server for products

---
+++ apps/products-subgraph/src/index.ts
@@ -0,0 +1,41 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+  type Product {
+    name: String
+    description: String
+  }
+
+  type Query {
+    products: [Product]
+  }
+`;
+
+const products: Array<Product> = [
+  {
+    name: "Bakkersrekje",
+    description: "Het ultieme bakelement",
+  },
+  {
+    name: "Accubak",
+    description: "Een geweldige accubak",
+  },
+];
+
+const resolvers = {
+  Query: {
+    products: () => products,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
