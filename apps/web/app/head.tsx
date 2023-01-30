986a640f10a5bcd6b5dbb4b09134e5208246912b
Alex Schouls <alex.schouls@valtech.com>
2023-01-30 11:14:50 +0100

    feat(web): removed Bulma

--- apps/web/app/head.tsx
+++ apps/web/app/head.tsx
@@ -4,12 +4,7 @@ export default function Head() {
   return (
     <>
       <title></title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link
-        rel="stylesheet"
-        precedence="default"
-        href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1"  />
     </>
   );
 }
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Navbar from './components/navbar';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
       <body>
-        <Navbar />
         {children}
       </body>
     </html>
