fcc1423ca0ddd86a77a33e24e7cd3fd3b942fe52
Alex Schouls <alex.schouls@valtech.com>
2023-01-30 10:35:35 +0100

    feat(web): added Bulma

--- apps/web/app/head.tsx
+++ apps/web/app/head.tsx
@@ -4,7 +4,12 @@ export default function Head() {
     return (
         <>
             <title></title>
-            <meta name="viewport" content="width=device-width, initial-scale=1"  />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link
+                rel="stylesheet"
+                precedence="default"
+                href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+            />
         </>
     );
 }
