7c3496ac1d7bdbc9fb8712b80ba74fe325641aec
Alex Schouls <alex.schouls@valtech.com>
2023-01-30 11:04:47 +0100

    feat(repo): updated ESLint config

--- apps/web/app/head.tsx
+++ apps/web/app/head.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 export default function Head() {
-    return (
-        <>
-            <title></title>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link
-                rel="stylesheet"
-                precedence="default"
-                href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
-            />
-        </>
-    );
+  return (
+    <>
+      <title></title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+        rel="stylesheet"
+        precedence="default"
+        href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+      />
+    </>
+  );
 }
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './components/navbar';
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <head />
-            <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head />
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
 }
