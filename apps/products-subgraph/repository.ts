830cdec559f696733a89ff0a3154f72a797cafc7
Alex Schouls <alex.schouls@valtech.com>
2023-01-31 13:14:37 +0100

    feat(products): removed Prisma in favor of lowdb

--- apps/products-subgraph/repository.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '../../node_modules/.prisma/client';
-
-export async function GetProducts(): Promise<Array<Product>> {
-  const prismaClient = new PrismaClient();
-
-  return [];
-}
